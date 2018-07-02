f0bf163bc8d6ec86d8b2adf94056ffe762a17e9a
Gary Boyd <eponick@gmail.com>
2018-07-02 13:54:44 -0400

    Test encode returns string, fail

--- test_encode.rb
+++ test_encode.rb
@@ -1,8 +1,12 @@
 require 'minitest/autorun'
-require_relative 'fetch_code.rb'
+require_relative 'encode.rb'
 
 class TestFetchCode < Minitest::Test
   def test_assert_1_equals_1
     assert_equal(1, 1)
+  end
+
+  def test_encode_returns_string
+    assert_equal(String, roman_encode(""))
   end
 end
