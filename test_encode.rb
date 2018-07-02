3daf7be703a09e2812af79d1d7fc8255916e3a71
Gary Boyd <eponick@gmail.com>
2018-07-02 13:58:15 -0400

    Test encode shifts by 5, fail

--- test_encode.rb
+++ test_encode.rb
@@ -1,12 +1,16 @@
 require 'minitest/autorun'
 require_relative 'encode.rb'
 
-class TestFetchCode < Minitest::Test
+class TestRomanEncode < Minitest::Test
   def test_assert_1_equals_1
     assert_equal(1, 1)
   end
 
   def test_encode_returns_string
     assert_equal(String, roman_encode("").class)
+  end
+
+  def test_encode_shifts_by_5
+    assert_equal('f', roman_encode('a'))
   end
 end
