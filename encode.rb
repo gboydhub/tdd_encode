f73d1602037b978c41e10ea5e56b38b660f93c21
Gary Boyd <eponick@gmail.com>
2018-07-02 14:16:17 -0400

    Test decode unshifts, fail

--- encode.rb
+++ encode.rb
@@ -6,3 +6,12 @@ def roman_encode(msg)
     end
     msg
 end
+
+def roman_decode(msg)
+    count = 0
+    msg.each_char do |c|
+        msg[count] = (c.ord - 5).chr
+        count += 1
+    end
+    msg
+end
